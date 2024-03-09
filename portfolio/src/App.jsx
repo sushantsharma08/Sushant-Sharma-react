69b3710d7bbe278e13e1076ec53e527367319b86
Sushant Sharma <88335357+sushantsharma08@users.noreply.github.com>
2024-03-10 02:37:46 +0530

    navbar fixed

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -12,7 +12,7 @@ function App() {
   return (
     <>
       <Router>
-        <div><Navbar /></div>
+        <Navbar />
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/Education' element={<Education />} />
--- portfolio/src/Components/Navbar.jsx
+++ portfolio/src/Components/Navbar.jsx
@@ -5,10 +5,10 @@ import {Link } from 'react-router-dom';
 const Navbar = () => {
   return (
     <div>
-      <Link to="/" >Home</Link>
-      <Link to="/Education" >Education</Link>
-      <Link to="/Skills" >Skills</Link>
-      <Link to="/projects" >Projects</Link>
+      <span >  <Link style={{textDecoration:"none", color:"inherit"}}  to="/" >Home</Link></span>
+      <span >  <Link style={{textDecoration:"none", color:"inherit"}}  to="/Education" >Education</Link></span>
+      <span >  <Link style={{textDecoration:"none", color:"inherit"}}  to="/Skills" >Skills</Link></span>
+      <span >  <Link style={{textDecoration:"none", color:"inherit"}}  to="/projects" >Projects</Link></span>
     </div>
   )
 }
