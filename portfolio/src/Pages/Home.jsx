5cf7d8c2fff5a7f69f6b57858c91a80e5386d830
Sushant Sharma <88335357+sushantsharma08@users.noreply.github.com>
2024-03-28 02:01:46 +0530

    tailwind added

--- portfolio/src/Pages/Home.jsx
+++ portfolio/src/Pages/Home.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <h1 className="text-3xl font-bold underline">
+        Home
+      </h1>
+    </div>
   )
 }
 
