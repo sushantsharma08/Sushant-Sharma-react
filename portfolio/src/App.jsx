76c9d7b4cc56c07f6e848735df08d4cfda7e495d
Sushant Sharma <88335357+sushantsharma08@users.noreply.github.com>
2024-03-30 01:51:46 +0530

    init project

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,15 +1,11 @@
-import { useState } from 'react'
 import './App.css'
 import Navbar from './Components/Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-    <div>
-      <Navbar/>
-    </div>
+    <Navbar/>
     </>
 
   )
