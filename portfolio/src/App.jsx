e597f1252b0d33e363564530aff00a5decd41f18
Sushant Sharma <88335357+sushantsharma08@users.noreply.github.com>
2024-03-30 02:18:06 +0530

    updated navbar

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,11 +1,25 @@
 import './App.css'
 import Navbar from './Components/Navbar'
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from './Pages/Home';
+import Education from './Pages/Education';
+import Skills from './Pages/Skills';
+import Projects from './Pages/Projects';
+
 
 function App() {
 
   return (
     <>
-    <Navbar/>
+      <Router>
+        <div><Navbar /></div>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/Education' element={<Education />} />
+          <Route path='/Skills' element={<Skills />} />
+          <Route path='/projects' element={<Projects />} />
+        </Routes>
+      </Router>
     </>
 
   )
--- portfolio/src/Components/Navbar.jsx
+++ portfolio/src/Components/Navbar.jsx
@@ -1,28 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route,Link } from 'react-router-dom';
-import Home from '../Pages/Home';
-import Education from '../Pages/Education';
-import Skills from '../Pages/Skills';
-import Projects from '../Pages/Projects';
+import {Link } from 'react-router-dom';
 
 
 const Navbar = () => {
   return (
     <div>
-    <Router>
-      <div>
-        <Link to="/" >Home</Link>
-        <Link to="/Education" >Education</Link>
-        <Link to="/Skills" >Skills</Link>
-        <Link to="/projects" >Projects</Link>
-      </div>
-        <Routes>
-          <Route path='/' element={<Home/>} />
-          <Route path='/Education' element={<Education/>} />
-          <Route path='/Skills' element={<Skills/>} />
-          <Route path='/projects' element={<Projects/>} /> 
-        </Routes>
-      </Router>
+      <Link to="/" >Home</Link>
+      <Link to="/Education" >Education</Link>
+      <Link to="/Skills" >Skills</Link>
+      <Link to="/projects" >Projects</Link>
     </div>
   )
 }
