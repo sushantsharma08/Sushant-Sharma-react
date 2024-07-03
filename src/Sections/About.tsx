bee8de40fe679921d6ed6960611d475a76e7ecb3
Sushant Sharma <88335357+sushantsharma08@users.noreply.github.com>
2024-07-04 03:19:43 +0530

    2 sec remaining

--- src/Sections/About.tsx
+++ src/Sections/About.tsx
@@ -3,13 +3,12 @@ import { Box } from "@mui/material"
 const About = () => {
   return (
     <div className={`individual_sections`} id="About">
-      {/* <div className="fewLines"> */}
       <div className="heading">
         <h1 >About Me</h1>
       </div>
 
       <Box className="content" >
-        <p style={{ fontSize: "larger" }}>
+        <p style={{ fontSize: "larger", lineHeight: "35px" }}>
 
           A <b className="highlighted_about"> Computer Science Engineering Graduate </b>, A tech enthusiast and a fitness geek,
           <br />who likes to play and learn in my free time.
@@ -17,13 +16,6 @@ const About = () => {
           <br />
           I work with javascript and its frameworks to build   end-to-end web projects  integrated <br />
           with databases and deployed using cloud based services like vercel and github.
-
-          {/* I mentor and inspire my juniors and friends, help them advance their skills providing a
-collaborative
-and innovative environment. <br />
-I stay up-to-date with cutting-edge technologies and industry trends. I remain at the forefront of
-software
-engineering practices. */}
           <br /><br />
           My goal is to make a meaningful impact in the field of software engineering, <br />
           both in terms of delivering exceptional software solutions  and nurturing the growth <br /> of the individuals
@@ -35,7 +27,6 @@ engineering practices. */}
           along with.
         </p>
       </Box>
-      {/* </div> */}
     </div>
   )
 }
